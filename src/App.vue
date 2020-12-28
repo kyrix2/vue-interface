59ebfcfb9697843b0597622c466aa04c94375e31
kyrix <kefioussama85@gmail.com>
2020-12-28 19:22:18 +0100

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+      <add-appointment @add="addItem"/>
+      <searchapt @searchRecords="searchAppointments" />
+      <appointments-list :appointments="filterApts" @remove="removeItem" @edit="editItem"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Searchapt from './components/Searchapt.vue';
+import AddAppointment from './components/AddAppointment';
+import AppointmentsList from './components/AppointmentsList';
+import axios from "axios";
+import _ from "lodash";
+
+
 
 export default {
-  name: 'App',
+  name: 'MainApp',
+  data: function(){
+    return{
+      title: "Appointment List",
+      appointments: [],
+      aptIndex: 0,
+      searchTerms: "",
+      filterKey: "petName",
+      filterDir: "asc"
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AppointmentsList,
+    AddAppointment,
+    Searchapt
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.json")
+      .then(response => (this.appointments = response.data.map(item => {
+        item.aptId = this.aptIndex;
+        this.aptIndex++;  
+        return item
+      })));
+  },
+  computed: {
+    searchedApts: function() {
+      return this.appointments.filter(item => {
+        return(
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase()) 
+        );
+      });
+    },
+    filterApts: function() {
+        return _.orderBy(
+            this.searchedApts,
+            item => {
+              return item[this.filterKey].toLowerCase();
+            }, this.filterDir
+        )
+    }
+  },
+  methods: {
+    searchAppointments: function(terms) {
+      this.searchTerms = terms;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    removeItem: function(apt){
+      this.appointments = _.without(this.appointments, apt);
+    },
+
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      })
+      this.appointments[aptIndex] [field] = text;
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
